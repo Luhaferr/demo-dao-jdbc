d4ea1092ce3e0877cd13f4c1612b63506c3884dd
Luiz Ferreira <luhaferr@gmail.com>
2024-09-24 21:43:14 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 //classe auxiliar responsável por instanciar os Daos
 public class DaoFactory {
     //todo: entender melhor sobre essa lógica
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
