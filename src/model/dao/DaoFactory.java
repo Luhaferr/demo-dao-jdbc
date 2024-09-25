a513e6edd425b45941e0407d87baf26dfd08a2fc
Luiz Ferreira <luhaferr@gmail.com>
2024-09-25 18:03:57 -0300

    Reusing instantiation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -5,7 +5,10 @@ import model.dao.impl.SellerDaoJDBC;
 
 //classe auxiliar responsável por instanciar os Daos
 public class DaoFactory {
-    //todo: entender melhor sobre essa lógica
+    /*
+    método estático do tipo SellerDao(interface) para criar um objeto de acesso a dados que retorna
+    um novo objeto Seller já conectado ao BD. Por isso é obrigado a passar uma conexão como argumento.
+     */
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
     }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -11,10 +11,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
+//é a classe que implementa a interface SelleDao
 public class SellerDaoJDBC implements SellerDao {
     private Connection conn;
 
+    //construtor com argumento tipo Connection para forçar injeção de dependência
     public SellerDaoJDBC(Connection conn) {
         this.conn = conn;
     }
@@ -36,7 +37,9 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
+        //objeto para executar consultas SQL com parâmetros.
         PreparedStatement st = null;
+        //armazena e manipula os resultados da consulta SQL feitas pelo PreparedStatement
         ResultSet rs = null;
         try {
             st = conn.prepareStatement(
@@ -44,19 +47,25 @@ public class SellerDaoJDBC implements SellerDao {
                     + "FROM seller INNER JOIN department "
                     + "ON seller.DepartmentId = department.Id "
                     + "WHERE seller.Id = ?");
+            //substituindo o placeholder
             st.setInt(1, id);
+
+            //rs recebe o resultado da execução da consulta
             rs = st.executeQuery();
+
+            /*
+            o resultset traz resultados em forma de tabela, como usamos POO, a lógica abaixo cria um objeto do tipo Seller
+            associado a outro objeto com os dados do departamento dele
+
+            o if é pra existe algum resultado, ou seja, se existe algum vendedor com esses parâmetros
+            se não houver, ele retorna null/não existe vendedor com esse Id.
+            o next é para continuar navegando pelos dados da tabela para instanciar o objeto
+             */
             if (rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                //instanciando departamento
+                Department dep = instantiateDepartment(rs);
+                //instanciando vendedor
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
             return null;
@@ -64,10 +73,29 @@ public class SellerDaoJDBC implements SellerDao {
         catch (SQLException e) {
             throw new DbException(e.getMessage());
         }
+        //fechamento dos recursos
         finally {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+    //método pra instanciar department e poder reusar esse método quando necessário
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
+    }
+    //método pra instanciar seller e poder reusar esse método quando necessário
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
     }
 
     @Override
