420593750110dbc04345f7a413215f693955e84c
Luiz Ferreira <luhaferr@gmail.com>
2024-09-26 18:28:46 -0300

    Insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -6,10 +6,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,9 +21,54 @@ public class SellerDaoJDBC implements SellerDao {
         this.conn = conn;
     }
 
+    //método para inserir um novo Seller no Banco de dados
     @Override
     public void insert(Seller obj) {
+        //objeto para executar consultas SQL com parâmetros.
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
 
+            //substituição dos placeholders
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+
+            //variável recebendo a execução do comando SQL e contabilizando número de linhas afetadas
+            int rowsAffected = st.executeUpdate();
+
+            //lógica para verificar quantas linhas foram afetadas e atribuir o ID aos novos objetos
+            if (rowsAffected > 0) {
+                //recebe as chaves geradas
+                ResultSet rs = st.getGeneratedKeys();
+                //havendo mais de um...
+                if (rs.next()) {
+                    //variavel id recebe o ID(coluna 1 no Banco de dados)
+                    int id = rs.getInt(1);
+                    //atribui o id recebido acima no objeto Seller
+                    obj.setId(id);
+                }
+                //fechamento do recurso ResultSet
+                DB.closeResultSet(rs);
+            }
+            else {
+                //exceção caso não hajam linhas afetadas
+                throw new DbException("Unexpected error! No rows affected!");
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        //fechamento do recurso Statement
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
