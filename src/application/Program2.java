8584d203240b19fd31c5f75ba413d758d96d8bd3
Luiz Ferreira <luhaferr@gmail.com>
2024-09-27 13:50:13 -0300

    Create DepartmentDaoJDBC and insert and update implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,22 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== Test 1: Department insert ===");
+        Department department = new Department(null, "Music");
+        departmentDao.insert(department);
+        System.out.println("Inserted! New id = " + department.getId());
+
+        System.out.println("\n=== Test 2: Department update ===");
+        departmentDao.insert(department);
+    }
+}
