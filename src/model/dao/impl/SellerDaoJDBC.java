ea927c2d3c77d8572da4d6e7dd5a1845975a41f7
Luiz Ferreira <luhaferr@gmail.com>
2024-09-26 23:24:01 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -71,9 +71,34 @@ public class SellerDaoJDBC implements SellerDao {
         }
     }
 
+    //método para atualizar os dados de um Seller no Banco de dados
     @Override
     public void update(Seller obj) {
+        //objeto para executar consultas SQL com parâmetros.
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                        + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                        + "WHERE Id = ?");
 
+            //substituição dos placeholders
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        //fechamento do recurso Statement
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
